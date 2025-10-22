c3a6a8ec28750dcfebc28a3a6ff6a0f2f3ee7124
Pei Mu <ds1231h@gmail.com>
2025-10-22 17:47:22 +0100

    upload ssb skew 100 performance data

--- measure/measure_ssb_skew.sh
+++ measure/measure_ssb_skew.sh
@@ -11,18 +11,18 @@ rm -rf compile.log
 echo "official" 2>&1|tee -a compile.log
 cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=0 VERBOSE=1 make >> compile.log 2>&1 && cd measure && bash ./hyperfine_in_mem_ssb_skew.sh official nan $1
 
-############################# estimated stats #############################
-echo "query_split with join_order_optimization before query_split, with estimated stats" 2>&1|tee -a compile.log
-cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=0 ENABLE_SPECIFY_EST_STAT=1 VERBOSE=1 make >> compile.log 2>&1 && cd measure && bash ./hyperfine_in_mem_ssb_skew.sh query_split js_wo_stats_stats $1
+############################## estimated stats #############################
+#echo "query_split with join_order_optimization before query_split, with estimated stats" 2>&1|tee -a compile.log
+#cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=0 ENABLE_SPECIFY_EST_STAT=1 VERBOSE=1 make >> compile.log 2>&1 && cd measure && bash ./hyperfine_in_mem_ssb_skew.sh query_split js_wo_stats_stats $1
+#
+##echo "query_split with join_order_optimization after query_split, with estimated stats" 2>&1|tee -a compile.log
+##cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 ENABLE_SPECIFY_EST_STAT=1 VERBOSE=1 make >> compile.log 2>&1 && cd measure && bash ./hyperfine_in_mem_ssb_skew.sh query_split rsj_wo_stats_stats $1
+############################## estimated stats #############################
 
-#echo "query_split with join_order_optimization after query_split, with estimated stats" 2>&1|tee -a compile.log
-#cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 ENABLE_SPECIFY_EST_STAT=1 VERBOSE=1 make >> compile.log 2>&1 && cd measure && bash ./hyperfine_in_mem_ssb_skew.sh query_split rsj_wo_stats_stats $1
-############################# estimated stats #############################
+#echo "query_split with join_order_optimization before query_split" 2>&1|tee -a compile.log
+#cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=0 VERBOSE=1 make >> compile.log 2>&1 && cd measure && bash ./hyperfine_in_mem_ssb_skew.sh query_split js $1
 
-echo "query_split with join_order_optimization before query_split" 2>&1|tee -a compile.log
-cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=0 VERBOSE=1 make >> compile.log 2>&1 && cd measure && bash ./hyperfine_in_mem_ssb_skew.sh query_split js $1
-
-#echo "query_split with join_order_optimization after query_split" 2>&1|tee -a compile.log
-#cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 VERBOSE=1 make >> compile.log 2>&1 && cd measure && bash ./hyperfine_in_mem_ssb_skew.sh query_split rsj $1
+echo "query_split with join_order_optimization after query_split" 2>&1|tee -a compile.log
+cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 VERBOSE=1 make >> compile.log 2>&1 && cd measure && bash ./hyperfine_in_mem_ssb_skew.sh query_split rsj $1
 
 mv compile.log ssb_skew_$1_result/.
